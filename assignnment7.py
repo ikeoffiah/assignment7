994a1dd7cb6729a0fc7ae4e7c2ca16019fad8f07
IKEOFFIAH PIUS <piuspio12@gmail.com>
2022-03-09 10:30:48 +0000

    first commit

---
+++ assignnment7.py
@@ -0,0 +1,16 @@
+employeeId="09875123"
+first_name = "Pius"
+last_name = "Ikeoffiah"
+basicSalary = 10000
+allowance= 300
+pensionScheme = 0.02*basicSalary
+tax = 0.01*basicSalary
+net_salary = basicSalary- (allowance+tax)
+print('Employee Id - ',employeeId)
+print('First Name - ',first_name)
+print('Last Name - ',last_name)
+print('Basic Salary - ','$', basicSalary)
+print('Allowance - ', '$',allowance)
+print('Pension Scheme - ','$', pensionScheme)
+print('Tax - ','$', tax)
+print('Net Salary - ','$',net_salary)
